0b43ec502132f7aa6870c78ed19caeee75b3337e
ahmed gamal <114102777+ahmedgamaleid@users.noreply.github.com>
2023-08-19 19:02:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,719 @@
+// window.alert("welcome");
+// window.document.getElementById("demo").innerHTML="welcome user";
+// console.log("hello developer");
+// var x =3; // declare --  initlization
+// var y =4;
+// var ahmed ="2";
+// var eid ="3";
+// var gamal ="a";
+// var moon =true;
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof(ahmed));
+// console.log(typeof(eid));
+// console.log(typeof(gamal));
+// console.log(typeof(moon));
+// console.log(eid-ahmed);
+// console.log((eid-ahmed)+x*y-eid);
+// console.log(eid-gamal);// nan
+
+
+
+
+// var username = prompt(" pls enter your name") ;
+// var age = prompt(" pls unter your age");
+// var age =Number(age)
+// var isgraduated = prompt(" pls unter your are graduated or not");
+// console.log(typeof(username));
+//  console.log(typeof(age));
+// console.log(typeof(isgraduated));
+// console.log(username);
+//  console.log(age);
+// console.log(isgraduated);
+
+
+
+
+
+
+
+
+
+
+// var leter = prompt("leter") 
+// if(leter == "a" )
+// {
+// alert( "a"  + "is vowl")
+// }
+// else if(leter == "e" )
+// {
+//     alert("e"   + "is vowl")
+// }
+// else if(leter == "i" )
+// {
+//     alert(  "i" + "is vowl")
+// }
+// else if(leter == "o" )
+// {
+//     alert( "o"  + "is vowl")
+// }
+// else if(leter == "u" )
+// {
+//     alert( "u"  + "is vowl")
+// }
+
+// else
+// {
+//     alert( leter + "is not vowl")
+// }
+
+
+
+
+
+
+
+
+// console.log(Number("ahmed"));
+
+
+
+// window.document.getElementById("name").innerHTML= "your name is :" +username;
+// window.document.getElementById("age").innerHTML= "your age is :" +age;
+// window.document.getElementById("salary").innerHTML= "your salary is :" + salary ;
+
+
+
+
+
+
+
+// #############################################
+
+// 1- Write a program that allow to user enter number then printit
+
+// var num =Number(num)
+// var num = prompt(" pls enter int num") ;
+// console.log(num);
+
+
+
+
+
+
+//2- Write a program that take number from user then print yes if that number can divide by 3and 4 otherwise print no 
+
+// var num = prompt(" pls enter int num") ;
+// if(num %3 ==0 && num %4 == 0)
+// {
+//  alert( "yes")
+// }
+// else 
+// {
+//     alert("no")
+// }
+
+
+
+
+
+
+
+// 3- Write a program that allows the user to insert 2 integers then print the max
+
+// var num =Number(fristnum);
+// var num =Number(secnum);
+// var  fristnum = prompt(" pls enter int fristnum") ;
+// var  secnum = prompt(" pls enter int secnum") ;
+
+// if (fristnum >secnum)
+// {
+  
+//     console.log("fristnum is amax" + fristnum);
+//      }
+// else
+// {
+
+// console.log("secnum is amax" + secnum);
+
+// }
+
+
+
+
+
+
+//4:Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+// var  num = prompt(" pls insert num")
+// var num =Number(num)
+// if (num > 0 )
+//  {
+  
+//      console.log("Output positive " );
+//       }
+//  else
+//  {
+
+//  console.log("Output negative" );
+
+//  }
+
+
+
+
+
+
+
+
+//5- Write a program that take 3 integers from user then print the max element and the min element.
+
+// var fristnum  =Number( fristnum);
+//  var  secnum =Number(secnum);
+//  var  thirnum =Number(thirnum);
+//  var   fristnum = prompt(" pls enter int frist num") ;
+//  var   secnum = prompt(" pls enter int sec num") ;
+//  var   thirnum = prompt(" pls enter int thir num") ;
+
+//max
+
+//  if (fristnum > secnum && fristnum > thirnum)
+//   {
+   
+//      console.log("fristnum is max " + fristnum);
+//        }
+//  else if (secnum  > fristnum && secnum > thirnum )
+//  {
+//  console.log("secnum is max " + secnum);
+ 
+//  }
+//  else{
+//     console.log("thir num is amax " + thirnum);
+//  }
+
+
+ //min
+
+//  if (fristnum < secnum && fristnum < thirnum)
+//  {
+  
+//     console.log("frist num is min " + fristnum);
+//       }
+// else if (secnum  < fristnum && secnum < thinum )
+// {
+// console.log("secnum is min " + secnum);
+
+// }
+// else{
+//    console.log("thir num is min " + thirnum);
+// }
+
+
+
+
+
+
+
+
+// 6: Write a program that allows the user to insert integer number then check If a number is oven or odd
+
+
+// var num  =Number( num );
+// var   num = prompt(" pls enter int num") ;
+// if (num % 2 == 0 )
+//   {
+  
+//      console.log("even " );
+//       }
+//   else
+//   {
+
+//   console.log("odd" );
+
+//   }
+
+
+
+
+
+
+
+// 7: Write a program that take character from user then if it is vowel chars (a,e,I,o,u) then print vowel otherwise print consonant
+
+// var leter = prompt("leter") 
+// if(leter == "a" )
+// {
+// alert( "a"  + "is vowl")
+// }
+// else if(leter == "e" )
+// {
+//     alert("e"   + "is vowl")
+// }
+// else if(leter == "i" )
+// {
+//     alert(  "i" + "is vowl")
+// }
+// else if(leter == "o" )
+// {
+//     alert( "o"  + "is vowl")
+// }
+// else if(leter == "u" )
+// {
+//     alert( "u"  + "is vowl")
+// }
+
+// else
+// {
+//     alert( elter + "is not vowl")
+// }
+
+
+
+
+
+
+// 8:Write aprogram that allows user to insert integer then print all numbers between 1 to that’s number
+
+// var num  =Number( num );
+// var   num = prompt(" pls enter int num") ;
+// for (num=1; num<=5; num++) {
+//     console.log(num ); 
+//   }
+
+
+
+
+
+
+//9- Write a program that allows userto insert integerthen print a multiplication table up to 12.
+
+// var num  =Number( num );
+// var   num = prompt(" pls enter int num") ;
+// var i  =Number( i );
+// var res  =Number( res );
+// for (i=1; i<=12; i++) {
+//        res=num*i;
+//        console.log( num + "*" + i + "=" + res ); 
+//     }
+
+
+
+
+
+//10- Writeaprogramthatallowstousertoinsertnumberthenprintallevennumbers between 1 to this number
+
+// var num  =Number( num );
+//  var   num = prompt(" pls enter int num") ;
+//  var i  =Number( i );
+
+//  for (i=1; i<=num; i++) {
+//     if (i % 2 == 0 )
+//       {
+//         console.log(i);
+//          }
+//        }
+
+
+
+
+
+
+
+
+
+//11- Write aprogram that take two integers then print the power
+
+//  var x  =Number( x );
+//   var   x = prompt(" pls enter int num") ;
+//    var y  =Number( y );
+//    var   y = prompt(" pls enter int num2") ;
+// var result = 1;
+// for( i = 0; i < y ; ++i)
+// {
+//     console.log(result *= x ) 
+// }
+
+//another ans
+// console.log(Math.pow(num, num2));
+
+
+
+
+
+
+//12- Write a program to enter marks of five subjects and calculate total, average and percentage. var num  =Number( num );
+
+  
+// var english = Number( english );
+// var chemistry = Number( chemistry );	
+// var  computers = Number( computers );	
+// var   physics = Number( physics );
+//  var  maths = Number( maths );
+
+//  var   english = prompt(" pls enter int num") ;
+
+//    var   chemistry = prompt(" pls enter int num2") ;
+
+//    var   computers = prompt(" pls enter int num3") ;
+
+//    var   physics = prompt(" pls enter int num4") ;
+
+//    var   maths = prompt(" pls enter int num5") ;
+
+//    var avg  =Number( ( english + chemistry + computers + physics + maths) / 5 );
+//    var prec  =Number( (( english + chemistry + computers + physics + maths)/500)*100 );
+//    var total  =Number( english + chemistry + computers + physics + maths);
+//    console.log("total="+total);
+//    console.log("avg="+ avg);
+//    console.log("prec="+ prec);
+
+
+
+
+
+    //  13-Write a program to input month number and print number of days in thatmonth. 
+
+
+
+
+//     var y  =Number( y );
+//   var   y = prompt(" pls enter int y") ;
+//        var m  =Number( m );
+//         var   m = prompt(" pls enter int m") 
+        
+// if(m==1)
+//     console.log("January="+y+"has 31 days");
+//     else if ( m == 2 )
+//     {
+//         if( y%4!=0)
+//             console.log("February="+y+"has 28 days");
+//             else
+//             console.log("February="+y+"has 29 days"); 
+       
+//     }
+//    else if(m==3 )
+//     {
+//         console.log("March="+ y +"has 29 days"); 
+//      }
+//      else if(m==4 )
+//     {
+//         console.log("April="+ y +"has 30 days"); 
+//      }
+//      else if(m==5 )
+//     {
+//         console.log("May="+ y +"has 31 days"); 
+//      }
+//      else if(m==6 )
+//     {
+//         console.log("June="+ y +"has 30 days"); 
+//      }
+//      else if(m==7 )
+//      {
+//          console.log("July="+ y +"has 31 days"); 
+//       }
+//       else if(m==8 )
+//       {
+//           console.log("August="+ y +"has 31 days"); 
+//        }
+//        else if(m==9 )
+//        {
+//            console.log("September="+ y +"has 30 days"); 
+//         }
+//         else if(m==10 )
+//        {
+//            console.log("October="+ y +"has 31 days"); 
+//         }
+//         else if(m==11 )
+//        {
+//            console.log("November="+ y +"has 30 days"); 
+//         }
+//         else if(m==12 )
+//        {
+//            console.log("December ="+ y +"has 31 days"); 
+//         }
+//         else  
+//         {
+//             console.log("the valid input for month form 1to 12"); 
+//          }
+
+
+
+// 14- Write a program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer , Find percentage and grade
+
+
+
+//  var Physics = Number( Physics );
+//  var  Chemistry = Number( chemistry );	
+//  var  Biology = Number( Biology );	
+//  var Mathematics = Number( Mathematics );
+//  var Computer = Number( Computer );
+
+//  var   Physics = prompt(" pls enter int Physics") ;
+
+//    var   chemistry = prompt(" pls enter int chemistry") ;
+
+//    var   Biology = prompt(" pls enter int Biology") ;
+
+//    var   Mathematics = prompt(" pls enter int Mathematics") ;
+
+//    var   Computer = prompt(" pls enter int Computer") ;
+//    var avg = ( Physics + chemistry + Biology + Mathematics + Computer ) / 5;
+
+
+
+//    if (avg >= 90) 
+//    console.log("grade = 'A'");
+   
+    
+    
+    
+//     else if (avg >= 80) {
+    
+//         console.log("grade = 'B'");
+    
+//     }
+    
+//     else if (avg >= 70) {
+    
+//         console.log("grade = 'C'");
+    
+//     }
+    
+//     else if (avg >= 60) {
+    
+//         console.log("grade = 'D'");
+    
+//     }
+    
+//     else if (avg >= 40) {
+    
+//         console.log("grade = 'E'");
+//     }
+    
+//     else {
+    
+//         console.log("grade = 'F'");
+    
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//15- Write a program to print total number of days in month
+
+
+
+//  var x =prompt(" number") ;
+// var   x = prompt(" pls enter num month") ;
+// switch(x)
+// 		{
+//             case ("1") :
+// 		  	case ("3") :
+// 			case ("5"): 	
+// 			case ("7") :
+// 			case ("8") :
+// 			case ("10") :
+// 			case ("12") :
+//                 console.log("31 Days in this Month");
+// 			  	break;
+//                   case ("4"):	
+//                   case ("6"):
+//                   case ("9"):
+//                   case ("11"):	
+//                   console.log("30 Days in this Month");
+//                       break;
+//                       case ("2") :
+//                         console.log("Either 28 or 29 Days in this Month");
+//                         break;
+//                     default:	
+//                     console.log("Please enter Valid Number between 1 to 12");
+//                     break;
+//                   }
+
+
+
+
+
+
+
+
+// 16- Write a program to check whether an alphabet is vowel or consonant
+
+
+
+// var   x =  prompt("char")  
+// switch(x)
+// 		{
+//             case "a" :
+//             case "e" :
+// 			case "i" :
+// 			case "o" :
+// 			case "u" :
+//                 console.log("is  vowl");  
+//                 break;
+//             default:	
+//                  console.log("is not vowl");
+//                 break;
+//         }
+
+
+
+
+
+
+
+
+// 17 : Write a program to find maximum between two numbers
+
+
+
+// var   x = prompt(" num 1") ;
+
+// var   y = prompt(" num 2") ;
+
+// switch(x > y)
+// {
+//     case 0: 
+   
+//     console.log( y  +"is Maximum number");
+//         break;
+//      case 1: 
+//      console.log( x +"is Maximum number");
+//         break;
+//         default:	
+//      console.log("non above");
+//         break;
+// }
+
+
+
+
+
+// 18 : Write a program to check whether a number is even or odd 
+
+// var num  =prompt("num");
+
+// switch(num % 2 == 0)
+// {
+// case 1:
+//     console.log("even " );
+//     break;
+//     case 0 :
+//         console.log("odd");  
+//     break;
+// default:
+//     console.log("error");  
+// }
+
+
+
+
+
+//19- Write a program to check whether a number is positive or negative or zero
+
+// var   x = prompt(" num 1") ;
+
+// switch(x > 0)
+//  		{
+//             case 1 :
+//                  console.log("positive");  
+//                  break;
+//                  case 0 :
+//                     switch(x < 0)
+//  		{
+//             case 1 :
+//                 console.log("negative");  
+//                 break;
+//                 case 0 :
+//                     console.log("zero");  
+//                     break;
+
+
+//         }
+//                break;
+//        }
+
+
+
+
+
+// 20- Write a program to create Simple Calculator
+
+// var   x =  prompt("char");
+// var num1 = Number( num1 );
+// var  num2 = Number( num2 );	
+//  var   num1 = prompt(" pls enter int num1") ;
+//  var   num2 = prompt(" pls enter int num2") ;
+//  switch (x) 
+//  {
+//      // If user enter +
+//     case '+':
+//     console.log(num1 + num2);  
+//         break;
+//          // If user enter -
+//         case '-':
+//          console.log( num1 + num2 );  
+//          break;
+//           // If user enter *
+//       case '*':
+//         console.log( num1 * num2 );  
+//        break;
+//     // If user enter /
+//    case '/':
+//      console.log( num1 / num2 );  
+//     break;
+//     // If the operator is other than +, -, * or /. 
+//     // error message will display
+//     default:
+//         console.log( "Error! operator is not correct" );  
+//         break;
+//  }
+
+
+
+// var student ={
+// studentname:"ahmed",
+// studentage:21,
+// gander:"male",
+// level:3,
+// graduated:false,
+// welcome:function(){
+//     alert("welcome");
+// },
+// adress:{
+// address:"5",
+// contery:"egypt"
+// }
+// }
+// console.log(student.adress.contery)
+// console.log(student.level)
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+//     loop:true
+//     autoplay:true
+//   });
+
+
+
+
+     
+       
